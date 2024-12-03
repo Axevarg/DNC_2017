17c1d4cb6d4df7f6e4e5808b75c7918fa1b12fb0
axebau <axel.vargas@dina.com.mx>
2024-12-03 15:11:57 -0600

    Uptade

--- GestionCapacitacion.aspx.vb
+++ GestionCapacitacion.aspx.vb
@@ -1068,7 +1068,7 @@ Public Class GestionCapacitacion
     End Sub
     Private Sub ddlCursoAutorizadoDNC_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ddlCursoAutorizadoDNC.SelectedIndexChanged
         Call ComportamientoLimpiaDncCursos()
-        Call ObtieneDatosCursosDNC()
+        Call ObtCursoGestionado()
     End Sub
     'Obtiene los Cursos de la DNC Autorizados
     Public Sub ObtieneDatosCursosDNC()
@@ -1118,7 +1118,6 @@ Public Class GestionCapacitacion
         hdIdGestionCapacitacion.Value = ddlConsultaCursosGestionados.SelectedValue
 
         Call ComportamientoLimpiaDncCursos()
-        Call ObtieneDatosCursosDNC()
         Call ObtCursoGestionado()
         Call Comportamientos()
 
